4992c322501759122cfa3b5d58552c48a0e3672a
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-06-01 08:36:23 -0500

    updates

--- coal_1915_1938.py
+++ coal_1915_1938.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.preprocessing import MinMaxScaler
+from sklearn import linear_model
 
 df = pd.read_excel('french_coal_production_1915_1938.xlsx')
 print(df.head())
@@ -16,7 +15,26 @@ y = df['metric_tons']
 print(y[0:5])
 
 plt.plot(X, y, color='cyan', marker='o')
-plt.title("French Coal Production 1870-1919")
+plt.title("French Coal Production 1915-1939")
 plt.xlabel("Year")
 plt.ylabel("Coal Production in Metric Tons")
+
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+print(regr.coef_, regr.intercept_)
+
+y_predict = regr.predict(X)
+plt.plot(X, y_predict, color='red')
+plt.title("French Coal Production Linear Regression")
+plt.xlabel("Year")
+plt.ylabel("Coal Production in Millions of Metric Tons")
+
+X_future = np.array(range(1918, 2022))
+X_future = X_future.reshape(-1, 1)
+
+future_predict = regr.predict(X_future)
+plt.plot(X_future, future_predict, color='green')
+plt.title("French Coal Production Linear Regression Predicted from 1915-1938 Data")
+plt.ylabel("Coal Production in Millions of Metric Tons")
+plt.xlabel("Year")
 plt.show()
