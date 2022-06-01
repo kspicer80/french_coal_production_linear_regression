d8a1a4fa31882345cdc7f4ffa3553b57d36ca941
kspicer80 <50631380+kspicer80@users.noreply.github.com>
2022-06-01 08:20:01 -0500

    updated excel sheet

--- excel_data_wrangling.py
+++ excel_data_wrangling.py
@@ -1,18 +1,8 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-df = pd.read_excel('french_coal_production.xlsx')
-print(df.dtypes)
-
-df['converted_units'] = df["num_with_units"].replace({"K":"*1e3", "M":"*1e6"}, regex=True).map(pd.eval).astype(int)
-df['converted_units'] = df['converted_units'].div(1000).round(1)
-print(df.head(50))
-
-sns.scatterplot(x='date', y='converted_units', data=df, hue='type')
-plt.show()
-
-
+from sklearn.preprocessing import MinMaxScaler
 
 def min_max_normalize(lst):
   minimum = min(lst)
@@ -22,5 +12,25 @@ def min_max_normalize(lst):
     normalized_num = (element - minimum) / (maximum - minimum)
     normalized.append(normalized_num)
   return normalized
+
+def to_million(x):
+    return abs(x/1000000)
+    
+df = pd.read_excel('french_coal_production.xlsx')
+print(df.dtypes)
+print(df.tail(25))
+
+df['converted_units'] = df["num_with_units"].replace({"K":"*1e3", "M":"*1e6"}, regex=True).map(pd.eval).astype(int)
+df['converted_units'] = df['converted_units'].apply(to_million)
+print(df.head(50))
+
+df_min_max_scaled = df.copy()
+#df_min_max_scaled['converted units'] = (df_min_max_scaled['converted_units'] - df_min_max_scaled['converted_units'].min()) / (df_min_max_scaled['converted_units'].max() - df_min_max_scaled['converted_units'].min())
+df_sklearn = df.copy()
+df_sklearn['converted_units'] = MinMaxScaler().fit_transform(np.array(df_sklearn['converted_units']).reshape(-1, 1))
+#df = df['converted_units'].apply(lambda x: min_max_normalize(x))
+sns.scatterplot(x='date', y='converted_units', data=df_sklearn, hue='type')
+plt.show()
+
 
 #print(min_max_normalize(release_dates))
